5cd66381a5582efafb0f21d2e11383b592a6527d
suzanne su <suhysu@gmail.com>
2018-10-25 23:56:52 -0700

    dbcontext

--- AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AsyncInn.Data
 {
@@ -10,7 +7,24 @@ namespace AsyncInn.Data
     {
         public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
         {
-                
+
         }
+
+        // For composite keys and shadow properties
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HotelRoom>().HasKey(
+                hr => new { hr.HotelID, hr.RoomID }
+                );
+            modelBuilder.Entity<RoomAmenity>().HasKey(
+                rm => new { rm.RoomID, rm.AmenityID }
+                );
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomAmenity> RoomAmenity { get; set; }
+        public DbSet<Amenity> Amenity { get; set; }
     }
 }
