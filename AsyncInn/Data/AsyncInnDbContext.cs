f63b9d3267510c7abac833beb099bc6ecb0de311
suzanne su <suhysu@gmail.com>
2018-10-25 21:44:56 -0700

    Scaffold

---
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
