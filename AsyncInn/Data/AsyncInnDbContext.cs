ed5b8f3d3f58743cce5875eb1ce55c8f42745967
suzanne su <suhysu@gmail.com>
2018-10-28 02:39:26 -0700

    not working

--- AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -19,6 +19,83 @@ namespace AsyncInn.Data
             modelBuilder.Entity<RoomAmenity>().HasKey(
                 rm => new { rm.RoomID, rm.AmenityID }
                 );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    ID = 1,
+                    Name = "The Ranch at Rock Creek",
+                    Address = "79 Carriage House Ln, Philipsburg, MT 59858",
+                    Phone = "877-786-1545"
+                },
+                new Hotel
+                {
+                    ID = 2,
+                    Name = "The Point in Saranac Lake",
+                    Address = "222 Beaverwood Rd, Saranac Lake, NY 12983",
+                    Phone = "518-891-5674"
+                },
+                new Hotel
+                {
+                    ID = 3,
+                    Name = "Brush Creek Ranch",
+                    Address = "66 Brush Creek Ranch Road, Saratoga, WY 82331",
+                    Phone = "307-327-5284"
+                },
+                new Hotel
+                {
+                    ID = 4,
+                    Name = "Twin Farms",
+                    Address = "452 Royalton Turnpike, Barnard, VT 05031",
+                    Phone = "802-234-9999"
+                },
+                new Hotel
+                {
+                    ID = 5,
+                    Name = "Mii amo",
+                    Address = "525 Boynton Canyon Rd, Sedona, AZ 86336",
+                    Phone = "844-244-9487"
+                }
+                );
+
+            modelBuilder.Entity<Room>().HasData(
+                new Room
+                {
+                    ID = 1,
+                    Name = "Single",
+                    Layout = 1
+                },
+                new Room
+                {
+                    ID = 2,
+                    Name = "Double",
+                    Layout = 2
+                },
+                new Room
+                {
+                    ID = 3,
+                    Name = "King",
+                    Layout = 1
+                },
+                new Room
+                {
+                    ID = 4,
+                    Name = "Twin",
+                    Layout = 1
+                },
+                new Room
+                {
+                    ID = 5,
+                    Name = "Studio",
+                    Layout = 0
+                },
+                new Room
+                {
+                    ID = 6,
+                    Name = "Queen",
+                    Layout = 1
+                }
+            );
         }
 
         public DbSet<Hotel> Hotels { get; set; }
