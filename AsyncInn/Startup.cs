5fb45daf1dc30554f24af5f7fdf2ae5f95bc79d1
suzanne su <suhysu@gmail.com>
2018-10-31 23:08:52 -0700

    create interface and service for amenity

--- AsyncInn/Startup.cs
+++ AsyncInn/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AsyncInn.Data;
+﻿using AsyncInn.Data;
+using AsyncInn.Interfaces;
+using AsyncInn.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +25,8 @@ namespace AsyncInn
             services.AddMvc();
             services.AddDbContext<AsyncInnDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IAmenity, AmenityService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
